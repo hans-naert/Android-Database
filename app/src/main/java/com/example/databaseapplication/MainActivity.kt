18a07d09f83c37390e31c3bdeddbd43a3b302a2a
Hans Naert <hans.naert@vives.be>
2023-03-17 12:01:01 +0100

    add data to database

--- app/src/main/java/com/example/databaseapplication/MainActivity.kt
+++ app/src/main/java/com/example/databaseapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.databaseapplication
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.databaseapplication.databinding.ActivityMainBinding
@@ -16,6 +17,25 @@ class MainActivity : AppCompatActivity() {
         val dbHelper = MyDatabaseHelper(this, "BookStore.db", 2)
         binding.createDatabaseButton.setOnClickListener {
             dbHelper.writableDatabase
+        }
+        binding.addDataButton.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            val values1 = ContentValues().apply {
+                // construct the first row of data
+                put("name", "The Da Vinci Code")
+                put("author", "Dan Brown")
+                put("pages", 454)
+                put("price", 16.96)
+            }
+            db.insert("Book", null, values1) // insert the first row of data
+            val values2 = ContentValues().apply {
+                // construct the second row of data
+                put("name", "The Lost Symbol")
+                put("author", "Dan Brown")
+                put("pages", 510)
+                put("price", 19.95)
+            }
+            db.insert("Book", null, values2) // insert the second row of data
         }
     }
 }
