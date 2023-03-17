5e98bc343aeaa40a04f39cb04e96b91397f90599
Hans Naert <hans.naert@vives.be>
2023-03-17 11:28:47 +0100

    add binding

---
+++ app/src/main/java/com/example/databaseapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.databaseapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.databaseapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
